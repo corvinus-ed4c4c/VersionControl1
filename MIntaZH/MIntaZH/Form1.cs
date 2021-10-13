dc3f31c2c34e83522e0173972e70093980f9ce81
corvinus-ed4c4c <andras.fazekas@stud.uni-corvinus.hu>
2021-10-13 20:37:28 +0200

    Excel

--- MIntaZH/MIntaZH/Form1.cs
+++ MIntaZH/MIntaZH/Form1.cs
@@ -9,19 +9,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace MIntaZH
 {
     public partial class Form1 : Form
     {
         List<OlympicResult> results = new List<OlympicResult>();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             Betölt("Summer_olympic_Medals.csv");
-           Feltölt();
+            Feltölt();
             Helyezes();
             
+
         }
 
         private void Betölt(string fileName)
@@ -49,7 +55,7 @@ namespace MIntaZH
 
                 }
             }
-            }
+        }
         private void Feltölt()
         {
             var years = (from r in results
@@ -61,33 +67,88 @@ namespace MIntaZH
         private int Helyezeskalk(OlympicResult or)
         {
             var CurrentPosition = 0;
-            int counter = 0;
+            
 
             var FilteredList = (from r in results
                                 where r.Year == or.Year && r.Country != or.Country
                                 select r
                                 );
 
-            foreach(var r in FilteredList)
+            foreach (var r in FilteredList)
             {
                 if (r.Medals[0] > or.Medals[0])
-                    counter++;
+                    CurrentPosition++;
                 if ((r.Medals[0] == or.Medals[0]) && (r.Medals[1] == or.Medals[1]))
-                    counter++;
-                if((r.Medals[0] == or.Medals[0]) && (r.Medals[1] == or.Medals[1]) && (r.Medals[2] > or.Medals[2]))
-                    counter++;
+                    CurrentPosition++;
+                if ((r.Medals[0] == or.Medals[0]) && (r.Medals[1] == or.Medals[1]) && (r.Medals[2] > or.Medals[2]))
+                    CurrentPosition++;
             }
 
 
-            return CurrentPosition +1;
+            return CurrentPosition + 1;
         }
         private void Helyezes()
         {
-            foreach(var r in results)
+            foreach (var r in results)
             {
                 r.Position = Helyezeskalk(r);
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateExcel();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            
+         
+    }
+    }
+     private void CreateExcel()
+        {
+            
+            var headers = new string[]
+            {
+                "Helyezés",
+                "Ország",
+                "Arany",
+                "Ezüst",
+                "Bronz"
+            };
+            for (int i = 0; i < headers.Length; i++) { 
+                xlSheet.Cells[1, i + 1] = headers[i];
+                   }
+            var FilteredResults = (from r in results
+                                   where r.Year == (int)comboBox1.SelectedItem
+                                   orderby r.Position
+                                   select r
+                                ) ;
+            int db = 2;
+            foreach(var r in FilteredResults)
+            {
+                xlSheet.Cells[db, 1] = r.Position;
+                xlSheet.Cells[db, 2] = r.Country;
+                xlSheet.Cells[db, 3] = r.Medals[0];
+                xlSheet.Cells[db, 4] = r.Medals[1];
+                xlSheet.Cells[db, 5] = r.Medals[2];
+                db++;
+            }
+        }
     }
 }
