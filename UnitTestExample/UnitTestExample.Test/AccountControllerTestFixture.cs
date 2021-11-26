323e0c4afcb1764e95882c37bd01c7777308b275
corvinus-ed4c4c <andras.fazekas@stud.uni-corvinus.hu>
2021-11-26 15:58:15 +0100

    jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -29,5 +29,24 @@ namespace UnitTestExample.Test
 
 
         }
+        [Test,
+            TestCase("Abcdefgh", false),
+            TestCase("ABCDEFG1", false),
+            TestCase("abcdefg1", false),
+            TestCase("Abcdef1", false),
+            TestCase("Abcdefg1", true)]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }  
+    
+    
     }
 }
