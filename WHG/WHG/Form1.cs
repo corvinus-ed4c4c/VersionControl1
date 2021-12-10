53830b04e4663c9a3346c5ee598ca472923cd9ab
corvinus-ed4c4c <andras.fazekas@stud.uni-corvinus.hu>
2021-12-10 15:29:04 +0100

    győztes kiv

--- WHG/WHG/Form1.cs
+++ WHG/WHG/Form1.cs
@@ -21,6 +21,7 @@ namespace WHG
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
         public Form1()
         {
             InitializeComponent();
@@ -51,6 +52,16 @@ namespace WHG
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
